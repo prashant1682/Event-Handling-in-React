644bad6e08fc798b6fd70569cafa40255625763f
prashant1682 <prashant009@gmail.com>
2020-12-31 13:32:27 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [onMouseOverCode, setOnMouseOverCode] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleOnMouseOver() {
+    setOnMouseOverCode(true);
+  }
+
+  function handleOneMouseOut() {
+    setOnMouseOverCode(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{
+          background: onMouseOverCode ? "black" : "white"
+        }}
+        onClick={handleClick}
+        onMouseOver={handleOnMouseOver}
+        onMouseOut={handleOneMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
